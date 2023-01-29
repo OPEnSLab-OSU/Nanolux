c071d5425d1b4717b49c210a3443b71901c4d2d2
David Mora <davidqmora@gmail.com>
2023-01-29 00:18:20 -0800

    Small changes to star customization for AudioLux.

--- WebApp/src/components/header/index.js
+++ WebApp/src/components/header/index.js
@@ -3,10 +3,10 @@ import { Link } from 'preact-router/match';
 import style from './style.css';
 
 const Header = () => (
-	<header class={style.header}>
-		<a href="/" class={style.logo}>
-			<img src="../../assets/preact-logo-inverse.svg" alt="Preact Logo" height="32" width="32" />
-			<h1>Preact CLI</h1>
+	<header className={style.header}>
+		<a href="/" className={style.logo}>
+			<img src="../../assets/cropped-cymaspace-logo-32x32.png" alt="CymaSpace Logo" height="32" width="32" />
+			<h1>AudioLux</h1>
 		</a>
 		<nav>
 			<Link activeClassName={style.active} href="/">
