d96f5323be9a80a085585b37e5e8526a4f119f7a
David Mora <davidqmora@gmail.com>
2023-02-01 20:55:28 -0800

    Redo Home screen.

--- WebApp/src/routes/home/index.js
+++ WebApp/src/routes/home/index.js
@@ -1,40 +1,17 @@
-import { h } from 'preact';
 import style from './style.css';
+import Patterns from "../../components/patterns";
+import NumericSlider from "../../components/numeric_slider";
+import SimpleSlider from "../../components/simple_slider";
 
 const Home = () => {
 	return (
-		<div class={style.home}>
-			<a href="https://preactjs.com">
-				<img src="../../assets/preact-logo.svg" alt="Preact Logo" height="160" width="160" />
-			</a>
-			<h1>Get Started Building PWAs with Preact-CLI</h1>
-			<section>
-				<Resource
-					title="Learn Preact"
-					description="If you're new to Preact, try the interactive tutorial to learn important concepts"
-					link="https://preactjs.com/tutorial/"
-				/>
-				<Resource
-					title="Differences to React"
-					description="If you're coming from React, check out our docs for where Preact differs"
-					link="https://preactjs.com/guide/v10/differences-to-react"
-				/>
-				<Resource
-					title="Learn Preact-CLI"
-					description="To learn more about Preact-CLI, read through the ReadMe & Wiki"
-					link="https://github.com/preactjs/preact-cli#preact-cli--"
-				/>
-			</section>
+		<div className={style.home}>
+			<Patterns />
+			<NumericSlider />
+			<NumericSlider />
+			<SimpleSlider />
+			<SimpleSlider />
 		</div>
-	);
-};
-
-const Resource = props => {
-	return (
-		<a href={props.link} class={style.resource}>
-			<h2>{props.title}</h2>
-			<p>{props.description}</p>
-		</a>
 	);
 };
 
