b8760ffe841c7d3abbbd6fd78b110f00182b47ca
David Mora <davidqmora@gmail.com>
2023-02-14 23:07:47 -0800

    Added history. Implemented exception handler to help in debugging.

--- Utilities/APIMock/main.py
+++ Utilities/APIMock/main.py
@@ -1,5 +1,9 @@
 from fastapi import FastAPI
+from fastapi.exceptions import RequestValidationError
 from fastapi.middleware.cors import CORSMiddleware
+from starlette import status
+from starlette.requests import Request
+from starlette.responses import JSONResponse
 
 from data_model import Settings, Pattern, Control
 
@@ -30,23 +34,40 @@ settings = Settings(
 )
 
 
+history = []
+
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request: Request, exc: RequestValidationError):
+    exc_str = f'{exc}'.replace('\n', ' ').replace('   ', ' ')
+    print(f'{exc}\n{request}')
+    # logger.error(request, exc_str)
+    content = {'status_code': 10422, 'message': exc_str, 'data': None}
+    return JSONResponse(content=content, status_code=status.HTTP_422_UNPROCESSABLE_ENTITY)
+
+
 @app.get("/api/settings")
 def get_settings():
+    history.append(f"Retrieved settings: {settings}\n")
     return settings
 
 
 @app.put("/api/settings")
-def put_settings(settings: Settings):
+def put_settings(new_settings: Settings):
+    global settings
+    settings = new_settings.copy()
+    history.append(f"Saved settings: {settings}\n")
     return {"message": "Settings saved."}
 
 
 @app.get("/api/patterns")
 def get_pattern_list():
+    history.append(f"Retrieved pattern list: {[p.name for p in Pattern]}\n")
     return [pattern.value for pattern in Pattern]
 
 
 @app.get("/api/pattern")
 def get_current_pattern():
+    history.append(f"Retrieved current pattern: {current_pattern}\n")
     return current_pattern
 
 
@@ -55,4 +76,13 @@ def put_current_pattern(request: Control):
     global current_pattern
     current_pattern = request.pattern
     print(f"New pattern: {current_pattern}")
+    history.append(f"Saved pattern: {current_pattern}\n")
     return {"message": "Pattern saved."}
+
+
+@app.get("/api/history")
+def get_history():
+    global history
+    saved_history = history.copy()
+    history = []
+    return saved_history
