e98369416cf4f0277c8d0aabfc18fa2ae466a1db
David Mora <davidqmora@gmail.com>
2023-03-17 20:22:46 -0700

    Rig base_url to Azure Mock API - redux

--- WebApp/src/utils/api.js
+++ WebApp/src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from 'redaxios';
 
-const BASE_URL = 'https://audioluxmockapi.azurewebsitesnet/';
+const BASE_URL = 'https://audioluxmockapi.azurewebsites.net';
 // const BASE_URL = 'http://localhost:8000';
 // const BASE_URL = 'http://192.168.4.1';
 
