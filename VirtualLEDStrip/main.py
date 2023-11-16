3dc090e85e70e66530961691a5a041140b5a8bac
Caleb Shilling <shillinc@oregonstate.edu>
2023-11-16 12:45:43 -0800

    Added more specific connection buttons and auto LED strip length detection. Should be final major change to v2 code aside from bugfixes.

--- VirtualLEDStrip/main.py
+++ VirtualLEDStrip/main.py
@@ -1,5 +1,4 @@
 import types
-import cv2
 import numpy as np
 import PySimpleGUI as sg
 import sim_helpers as sim
@@ -10,46 +9,83 @@ states.CONNECTING_STATE = 0
 states.CONNECTED_STATE = 1
 states.DISCONNECTED_STATE = 2
 states.EXITING_STATE = 3
+state_to_string = [
+    'Connecting',
+    'Connected',
+    'Disconnected',
+    'Exiting'
+]
 
 LED_HEIGHT = 1
 LED_WIDTH = 60
 PIXEL_LENGTH = 14
+CONNECT_BUTTON = "-CONNECT-"
+DISCONNECT_BUTTON = "-DISCONNECT-"
 
 # GUI Constants
 LOGO_SCALE = 10
-LOGO_PATH = 'assets/logo2.png'
+LOGO_PATH = sim.get_absolute_path('\\assets\logo2.png')
 
 sg.theme('SystemDefault')
 layout = [
     [sg.Image(LOGO_PATH, subsample=LOGO_SCALE)],
     sim.make_display_bar("HSV"),
     sim.make_display_bar("BGR"),
-    sim.make_display_bar("RGB")
+    sim.make_display_bar("RGB"),
+    [
+        sg.Button('Connect', key=CONNECT_BUTTON),
+        sg.Button('Disconnect', key=DISCONNECT_BUTTON),
+        sg.Text('', key="-STATUS-")
+    ]
 ]
 
 # Create the Window
 window = sg.Window('Window Title', layout)
 
+# Update the application window with new images.
+def update_bars(bgr, hsv, rgb):
+    window['-RGB-'].update(data=sim.mat_to_png(rgb))
+    window['-BGR-'].update(data=sim.mat_to_png(bgr))
+    window['-HSV-'].update(data=sim.mat_to_png(hsv))
+
 if __name__ == '__main__':
 
     # Application state setup
-    app_state = states.CONNECTING_STATE
+    app_state = states.DISCONNECTED_STATE
     SER = None
     connection_attempted = False
+
+    # Defaults the LED strip length to 60.
+    # Common LED strip length, divides well into
+    # 600.
+    strip_length = 60
+
+    # Ensure that the bars are drawn at least once
+    event, values = window.read(0)
+    blank = np.zeros((LED_HEIGHT, LED_WIDTH, 3), np.uint8)  # (B, G, R)   
+    blank = sim.resize_image(blank, 10, 30)
+    update_bars(blank, blank, blank)
 
     # Main application loop
     while app_state != states.EXITING_STATE:
 
+        # Check for events from the UI.
+        event, values = window.read(0)
+        if event == sg.WIN_CLOSED: # if user closes window or clicks cancel
+            app_state = states.EXITING_STATE
+
         # This image stores the BGR data for the application to display.
-        blank_image = np.zeros((LED_HEIGHT, LED_WIDTH, 3), np.uint8)  # (B, G, R)
+        bgr = np.zeros((LED_HEIGHT, sim.LED_BAR_TARGET_LENGTH, 3), np.uint8)  # (B, G, R)
 
         match app_state:
             case states.CONNECTING_STATE:
-                # If this is the first round of connections, give a notification
-                # print to console.
-                if not connection_attempted:
-                    print("Connecting")
-                    connection_attempted = True
+                # Set the disable state of both buttons
+                window[CONNECT_BUTTON].update(disabled=True)
+                window[DISCONNECT_BUTTON].update(disabled=False)
+  
+                # If the connection button is pressed again, disconnect.
+                if event == DISCONNECT_BUTTON:
+                    app_state = states.DISCONNECTED_STATE
 
                 # Attempt to connect to serial. If serial_setup returns a value
                 # that is not None, move to the connected state and give a
@@ -61,38 +97,47 @@ if __name__ == '__main__':
                     connection_attempted = False
 
             case states.CONNECTED_STATE:
-                # Try to grab serial data and transform the blank image using the BGR
-                # data from the AudioLux.
-                # If this fails for whatever reason, move to the disconnected state.
-                try:
-                    serial_data = SER.readline().decode("utf-8").split()
-                    blank_image = sim.update_image(blank_image, serial_data)
-                except:
+                # Set the disable state of both buttons
+                window[CONNECT_BUTTON].update(disabled=True)
+                window[DISCONNECT_BUTTON].update(disabled=False)
+
+                # If the connection button is pressed again, disconnect.
+                if event == DISCONNECT_BUTTON:
                     app_state = states.DISCONNECTED_STATE
 
+                # Try to grab serial data and transform the blank image using the BGR
+                # data from the AudioLux.
+                # If this fails for whatever reason, move to the disconnected state and
+                # disconnect from the AudioLux.
+                try:
+                    # Read in data from serial and process it into a bgr image.
+                    serial_data = SER.readline().decode("utf-8").split()
+                    bgr = sim.update_image(bgr, serial_data)
+                    hsv, rgb = sim.bgr_to_hsv_rgb(bgr)
+                    update_bars(bgr, hsv, rgb)
+                except:
+                    app_state = states.DISCONNECTED_STATE
+                    SER.close()
+                    SER = None
+
             case states.DISCONNECTED_STATE:
-                # If we are in the disconnected state, give a notification print, then
-                # immediately move to the connecting state.
-                print("Device Disconnected")
-                app_state = states.CONNECTING_STATE
+                # Set the disable state of both buttons
+                window[CONNECT_BUTTON].update(disabled=False)
+                window[DISCONNECT_BUTTON].update(disabled=True)
+
+                # If we are in the disconnected state, wait until the connect button
+                # is pressed before moving to the connecting state.
+                if event == CONNECT_BUTTON:
+                    app_state = states.CONNECTING_STATE
+                    
             case _:
                 # If we are in this state, there is an issue with state control.
                 # If this print occurs, there is a code issue that must be resolved.
-                print("Error, closing.")
                 app_state = states.EXITING_STATE
-
-        # Generates images based on different LED strip color arrangements.        
-        blank_image = sim.resize_image(blank_image, 10, 30)
-        hsv_img = cv2.cvtColor(blank_image, cv2.COLOR_BGR2HSV)
-        rgb_img = cv2.cvtColor(blank_image, cv2.COLOR_BGR2RGB)
-
-        event, values = window.read(0)
-        if event == sg.WIN_CLOSED: # if user closes window or clicks cancel
-            app_state = states.EXITING_STATE
-            break
-
-        window['-RGB-'].update(data=sim.mat_to_png(rgb_img))
-        window['-BGR-'].update(data=sim.mat_to_png(blank_image))
-        window['-HSV-'].update(data=sim.mat_to_png(hsv_img))
+        
+        # Update the graphs on the UI that display the virtual LED strips
+        # if not in the process of exiting.
+        if app_state != states.EXITING_STATE:
+            window['-STATUS-'].update(state_to_string[app_state])
 
         
--- VirtualLEDStrip/sim_helpers.py
+++ VirtualLEDStrip/sim_helpers.py
@@ -4,9 +4,26 @@ import serial.tools.list_ports
 import PySimpleGUI as sg
 from PIL import Image
 import io
+import pathlib
+import math
 
 NO_DEBUG = False
 DEBUG = True
+
+# The LED bar length will target 600 pixels.
+LED_BAR_TARGET_LENGTH = 600
+
+# Returns the size of each LED pixel given LED strip length
+# and the amount of overhang pixels.
+def calculate_pixel_width(led_count):
+
+    w = math.floor(LED_BAR_TARGET_LENGTH/led_count)
+    e = LED_BAR_TARGET_LENGTH - w*led_count
+
+    return w, e
+
+def get_absolute_path(relative_path):
+    return pathlib.Path(__file__).parent.resolve().__str__() + relative_path
 
 def make_display_bar(key):
     return [sg.Text(key + ": "), sg.Image(key="-" + key + "-")]
@@ -36,13 +53,28 @@ def resize_image(img, scale_x, scale_y):
     return resized
 
 def update_image(img, serial_data):
+
+    # Get the number of pixels per pixel.
+    try:
+        w, _ = calculate_pixel_width(len(serial_data))
+    # If this fails, the serial data is likely empty, so just resize
+    # and return.
+    except:
+        img = resize_image(img, 1, 30)
+        return img
+
     led_values = []
+    # Enumerate through the serial data and split out each pixel.
     for i, led_value in enumerate(serial_data):
         values = led_value.split(",")
         try:
-            img[:, i] = (int(values[2]), int(values[1]), int(values[0]))  # (B, G, R)
+            # Apply the recieved pixel color to all corresponding image
+            # pixels.
+            for j in range(i*w, (i+1)*w):
+                img[:, j] = (int(values[2]), int(values[1]), int(values[0]))  # (B, G, R)
         except:
             pass
+    img = resize_image(img, 1, 30)
     return img
 
 # Citation:
@@ -52,3 +84,10 @@ def mat_to_png(mat):
     bio = io.BytesIO()
     img.save(bio, format="PNG")
     return bio.getvalue()
+
+# Convert a BGR image to an HSV and an RGB image.
+def bgr_to_hsv_rgb(bgr):
+    hsv = cv2.cvtColor(bgr, cv2.COLOR_BGR2HSV)
+    rgb = cv2.cvtColor(bgr, cv2.COLOR_BGR2RGB)
+    return hsv, rgb
+    
