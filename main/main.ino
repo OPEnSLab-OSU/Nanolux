c21e6a836daecfa280d848e2a8f6f6ee869d11bd
Caleb Shilling <shillinc@oregonstate.edu>
2023-11-28 21:36:19 -0800

    Fix: reset LED data and history when switching to/from strip splitting mode.

--- main/main.ino
+++ main/main.ino
@@ -33,6 +33,8 @@ arduinoFFT FFT = arduinoFFT();
 CRGB leds[NUM_LEDS];               // Buffer (front)
 CRGB hist[NUM_LEDS];               // Buffer (back)
 
+CRGB empty_strip[NUM_LEDS]; 
+
 // LED arrays and buffers for strip splitting.
 CRGB half_leds_buf[HALF_NUM_LEDS];
 int virtual_led_count = NUM_LEDS;
@@ -64,6 +66,7 @@ int F2arr[20];
 int formant_pose = 0;
 
 volatile bool isStripSplitting = false;
+bool previousState = isStripSplitting;
 
 uint8_t genre_smoothing[10] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 int genre_pose = 0;
@@ -245,6 +248,16 @@ void loop() {
     #ifdef LAYER_PATTERNS
       layer_patterns();
     #else
+
+      // If the last run was not using strip splitting, clear all buffers
+      // and histories.
+      if(isStripSplitting != previousState){
+        previousState = isStripSplitting;
+        memset(leds, 0, sizeof(CRGB) * NUM_LEDS);
+        memset(hist, 0, sizeof(CRGB) * NUM_LEDS);
+        histories[0] = Pattern_History();
+        histories[1] = Pattern_History();
+      }
 
       if(isStripSplitting){
         // Set the virtual LED strip length
